bf95d65bbcfd923e08dac3890e0ea573443351ae
Lucille <lucille.gillard@macroprog.com>
2021-01-05 11:37:57 +0100

    Add: send messages inputs

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -6,15 +6,22 @@
 
     <b-sidebar id="sidebar-backdrop" :backdrop-variant="variant" backdrop right shadow title="Fil de discussion">
       <div class="px-3 py-2">
-        
+        <MessagesBox/>
+        <label class="d-block" for="message">Message :</label>
+        <input id="message" v-model="message" placeholder="Entrer votre message ici" type="text"/>
+        <button v-b-tooltip.hover class="pointer" title="Envoyer" type="submit" @click="sendMessage">
+          <b-icon-arrow-right></b-icon-arrow-right>
+        </button>
       </div>
     </b-sidebar>
   </div>
 </template>
 
 <script>
+import MessagesBox from '@/components/MessagesBox';
+
 export default {
-  name: 'Chat', data()
+  name:       'Chat', components: { MessagesBox }, data()
   {
     return {
       variant:  'dark',
@@ -28,7 +35,15 @@ export default {
                  'danger',
                  'warning',
                  'info'],
+      message:  '',
     };
+  }, methods: {
+    sendMessage()
+    {
+      // TODO send message by using websocket
+      console.log('message were sent : ' + this.message);
+      this.message = '';
+    },
   },
 };
 </script>
